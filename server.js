696f0957422fbe59c7086d545569cb494d1e337a
vedikagirhe <vedikagirhe@gmail.com>
2026-07-15 17:47:18 +0530

    Day 2: Create Home and Add Blog HTML pages

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send(`
-        <div style="
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 100px;
-        ">
-            <h1>📝 Simple Blog Manager</h1>
-            <h2>Full Stack Web Development Internship Project</h2>
-            <p>✅ Express Server is Running Successfully</p>
-            <p><strong>Day 1 – Environment Setup Completed</strong></p>
-        </div>
-    `);
-});
+// Serve HTML files from the public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
