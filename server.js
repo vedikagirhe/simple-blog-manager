23265d7642d95c7e2b476ae65be5305b8e11bdc2
vedikagirhe <vedikagirhe@gmail.com>
2026-07-18 15:58:07 +0530

    Day 5: Added Express GET and POST routes

--- server.js
+++ server.js
@@ -4,9 +4,35 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve HTML files from the public folder
+// Middleware
+app.use(express.json());
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// GET Route
+app.get("/", (req, res) => {
+    res.send("Welcome to Simple Blog Manager!");
+});
+
+// GET Blogs Route
+app.get("/blogs", (req, res) => {
+    res.json({
+        message: "GET request received successfully!"
+    });
+});
+
+// POST Blogs Route
+app.post("/blogs", (req, res) => {
+    console.log(req.body);
+
+    res.json({
+        message: "POST request received successfully!",
+        data: req.body
+    });
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
